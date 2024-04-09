f430e41b6cd6c7cb96ecc6dfe869acc876312c2c
bipiz <aaychuahan@algomau.ca>
2024-04-08 21:47:51 -0400

    Initial Commit

---
+++ backupdrives/backupdrivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  backupdrivesApp.swift
+//  backupdrives
+//
+//  Created by Bip_iz on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct backupdrivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
